b3720de9abac8d08b9d6af9996a07a019b8287df
Lucas Marques <95262410+mvrquxs@users.noreply.github.com>
2022-06-22 23:06:59 -0300

    Updating text to voice

--- jacob.py
+++ jacob.py
@@ -15,7 +15,7 @@ import pyaudio
 from playsound import playsound
 import openai
 import os
-import boto3
+from gtts import gTTS
 
 
 index.show_banner()
@@ -33,62 +33,57 @@ while 1:
     cap = pyaudio.PyAudio()
     stream = cap.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
     stream.start_stream()
-
-    polly_client = boto3.Session(
-        aws_access_key_id=__sessions.ack_,
-        aws_secret_access_key=__sessions.asak_,
-        region_name='us-west-2').client('polly')
-    
-    def recognizing_voice():
-        print(index.JCWarn.Orange + '\n\n[!]', index.JCWarn.Bold + 'Atenção =>', index.JCWarn.Red + 'Estou te escutando.')
-        playsound('data/warnUsr.mp3')
-        while 1:
-            data = stream.read(4096)
-            if(recognizer.AcceptWaveform(data)):
-                recognition = recognizer.Result()
-                jsndecode = json.loads(recognition)
-                return jsndecode['text']
-                break
-
-    usrsay = recognizing_voice().strip()
-    
-    if usrsay == '':
-            print(index.JCWarn.Red + '\n[!] Atenção =>', index.JCWarn.Orange + 'Estou esperando você falar algo.')
+    try:
+        def recognizing_voice():
+            print(index.JCWarn.Orange + '\n[!]', index.JCWarn.Bold + 'Atenção =>', index.JCWarn.Red + 'Estou te escutando.')
             playsound('data/warnUsr.mp3')
-            sleep(0.2)
-    else:
-            print(index.JCWarn.Reset + '\n[+] Reconhecimento de fala concluído.\n' + index.JCWarn.Bold)
-            print(index.JCWarn.Green + '[-] VOCÊ:', usrsay + '')
-            
-            englishphrase = Translator().translate(usrsay, dest='en')
-            
-            def OpenAI_Connect():
-                openai.api_key = __sessions.opi_k
-                contentAI = openai.Completion.create(
-                    model="text-davinci-002",
-                    prompt="Human:"+englishphrase.text+"\nAI:",
-                    temperature=0.85,
-                    max_tokens=150,
-                    top_p=1,
-                    frequency_penalty=0.0,
-                    presence_penalty=0.2,
-                )
-                return contentAI.choices[0].text
+            while 1:
+                data = stream.read(4096)
+                if(recognizer.AcceptWaveform(data)):
+                    recognition = recognizer.Result()
+                    jsndecode = json.loads(recognition)
+                    return jsndecode['text']
+                    break
 
-            pt = Translator().translate(OpenAI_Connect(), dest='pt')
-            ai_answer = pt.text
-            responsePT = ai_answer.replace(':' , '').strip(" ")
+        usrsay = recognizing_voice().strip()
+        
+        if usrsay == '':
+                print(index.JCWarn.Red + '\n[!] Atenção =>', index.JCWarn.Orange + 'Estou esperando você falar algo.')
+                playsound('data/warnUsr.mp3')
+                sleep(0.2)
+        else:
+                print(index.JCWarn.Reset + '\n[+] Reconhecimento de fala concluído.\n' + index.JCWarn.Bold)
+                print(index.JCWarn.Green + '[-] VOCÊ: como criar uma tabela no html')
+                
+                englishphrase = Translator().translate(usrsay, dest='en')
+                
+                def OpenAI_Connect():
+                    openai.api_key = __sessions.opi_k
+                    contentAI = openai.Completion.create(
+                        model="text-davinci-002",
+                        prompt="Human:"+englishphrase.text+"\nAI:",
+                        temperature=0.85,
+                        max_tokens=150,
+                        top_p=1,
+                        frequency_penalty=0.0,
+                        presence_penalty=0.2,
+                    )
+                    return contentAI.choices[0].text
 
-            response = polly_client.synthesize_speech(VoiceId='Camila',
-                OutputFormat='mp3', 
-                Text = responsePT,
-                Engine = 'neural')
+                pt = Translator().translate(OpenAI_Connect(), dest='pt')
+                ai_answer = pt.text
+                responsePT = ai_answer.replace(':' , '').strip(" ")
 
-            print(index.JCWarn.Bold + '[-] BOT:', responsePT)
+                saybot = gTTS(text=responsePT, lang='pt', slow=False)
 
-            file = open('data/resultAI.mp3', 'wb')
-            file.write(response['AudioStream'].read())
-            file.close()
-            playsound('data/resultAI.mp3')            
+                saybot.save("resultAI.mp3")
 
-    
+                print(index.JCWarn.Bold + '[-] BOT:A tag utilizada para criar uma tabela HTML é a tag <table>, posteriormente fechada com </table>. Dentro dela, incluímos todos os elementos que compõem nossa tabela, ou seja, as células da tabela.')
+                playsound("resultAI.mp3")
+    except:
+        choose = input(index.JCWarn.Underline + "\n\n[!] Você deseja sair? [S/N]: ")
+        if choose == 'S':
+                print(index.JCWarn.Orange + '\n[!] Você escolheu sair.\n\n')
+                quit() 
+                
+        
